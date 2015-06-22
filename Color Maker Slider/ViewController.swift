3772008f6657f31ae32d1ccad2ff8c9a4653c5fa
akumar <akumar@central1.com>
2015-06-22 13:03:52 -0700

    Initial Commit

---
+++ Color Maker Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Maker Slider
+//
+//  Created by Amit Kumar on 2015-06-22.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Color Maker SliderTests/Color_Maker_SliderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Color_Maker_SliderTests.swift
+//  Color Maker SliderTests
+//
+//  Created by Amit Kumar on 2015-06-22.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Color_Maker_SliderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
